fab4b0710f5752eb00d80bce19a9568d6683035d
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-21 15:01:56 +0530

    Add files via upload

---
+++ Static/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Static_constructor
+{
+    class geeks
+    {
+
+        
+        static geeks()
+        {
+
+            
+            Console.WriteLine("Static Constructor");
+        }
+
+        
+        public geeks(int i)
+        {
+            Console.WriteLine("Instance Constructor " + i);
+        }
+
+        
+        public string geeks_detail(string name, int id)
+        {
+            return "Name:" + name + " id:" + id;
+        }
+
+       
+        public static void Main()
+        {
+
+            
+            geeks obj = new geeks(1);
+
+            Console.WriteLine(obj.geeks_detail("GFG", 1));
+
+            
+            geeks obj1 = new geeks(2);
+
+            Console.WriteLine(obj1.geeks_detail("GeeksforGeeks", 2));
+        }
+    }
+}
+        
