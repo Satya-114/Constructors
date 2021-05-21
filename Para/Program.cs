6746d4f22fd07e404bf54ce105aaa821552e25c6
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-21 14:51:36 +0530

    Add files via upload

---
+++ Para/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Para_Constructor
+{
+    
+    
+        class paraconstrctor
+        {
+            public int a, b;
+            public paraconstrctor(int x, int y)  //
+            {
+                a = x;
+                b = y;
+            }
+        }
+        class MainClass
+        {
+            static void Main()
+            {
+                paraconstrctor v = new paraconstrctor(100, 175);     
+                
+                Console.WriteLine("value of a=" + v.a);
+                Console.WriteLine("value of b=" + v.b);
+                Console.Read();
+            }
+        }
+    }
+
+
