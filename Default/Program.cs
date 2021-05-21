a60ed504ff5a3c90a0bdce3cd0bfc10e4d25b6ae
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-21 14:46:49 +0530

    Add files via upload

---
+++ Default/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Default_constructors
+{
+    class addition
+    {
+      
+            int a, b;
+            public addition()   //default contructor  
+            {
+                a = 100;
+                b = 175;
+            }
+
+            public static void Main()
+            {
+                addition obj = new addition(); //an object is created , constructor is called  
+                Console.WriteLine(obj.a);
+                Console.WriteLine(obj.b);
+                Console.Read();
+            }
+        }
+    }
+
+   
