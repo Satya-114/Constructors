d3bea8d28c04b570bf4fa899d2447f892ed53ad9
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-21 15:21:47 +0530

    Add files via upload

---
+++ Copy/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Copy_constructors
+{
+    class employee
+    {
+        private string name;
+        private int age;
+        public employee(employee emp)   
+        {
+            name = emp.name;
+            age = emp.age;
+        }
+        public employee(string name, int age)   
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public string Details      
+        {
+            get
+            {
+                return " The age of " + name + " is " + age.ToString();
+            }
+        }
+    }
+    class empdetail
+    {
+        static void Main()
+        {
+            employee emp1 = new employee("Vithal", 23);    
+            employee emp2 = new employee(emp1);            
+            Console.WriteLine(emp2.Details);
+            Console.ReadLine();
+        }
+    }
+}
