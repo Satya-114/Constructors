ed48cccf38b0de0a00d33beaa4be5060e611ab3b
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-21 15:05:35 +0530

    Add files via upload

---
+++ Private/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Private_constructor
+{
+     public class Counter
+    {
+        private Counter()     
+        {
+        }
+        public static int currentview;
+        public static int visitedCount()
+        {
+            return ++currentview;
+        }
+    }
+    class viewCountedetails
+    {
+        static void Main()
+        {
+            
+            Console.WriteLine();
+            Counter.currentview = 500;
+            Counter.visitedCount();
+            Console.WriteLine("Now the view count is: {0}", Counter.currentview);
+            Console.ReadLine();
+        }
+    }
+}  
+        
